b108f9c89b9928d18e16e1585e8989ad3310c947
mfux <maxfuchs@protonmail.com>
2022-10-24 12:51:22 +0200

    python project starter

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     version="0.1",
     packages=["wsboyt"],
     package_dir={"": "src"},
-    install_requires=["pytest"],  # ToDo: move dependencies from requirements.txt
+    install_requires=["pytest==7.1.3", "black==22.10.0"],
 )
